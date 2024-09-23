391358321b7a3a6234d4f666b9f5d614bce05a93
Adeesha-Sandaruwan <it23551084@my.sliit.lk>
2024-09-23 19:58:52 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <footer>
+
+
+        <div id="containerr">
+            <img src="images\logo2.png" alt="Logo EdusparkOnline2" id="logo2">
+            <div id="footer-items">
+                <h3>Contact Us</h3>
+                <p>Email : info@EdusparkOnline.com</p>
+                <p>Phone : +94 117827682</p>
+                <p>Address : Eduspark Building, 9th lane , High level Road , Maharagama</p>
+            </div>
+            <div id="footer-items">
+                <h3>Quick Links</h3>
+                <ul id="list">
+                    <li><a href="#"><u>Home</u></a></li>
+                    <li><a href="#">Courses</a></li>
+                    <li><a href="#">Events</a></li>
+                    <li><a href="#">About Us</a></li>
+                    <li><a href="#">Contact Us</a></li>
+                    <li><a href="#">FAQ</a></li>
+                    <li><a href="#">Terms & Conditions</a></li>
+                    <li><a href="#">Privacy & Cookies</a></li>
+                </ul>
+            </div>
+
+            <div id="footer-items">
+                <h3>Follow Us</h3>
+                <li id="social"><a href="#"><i class="fab fa-facebook"></i></a></li>
+                <li id="social"><a href="#"><i class="fab fa-twitter"></i></a></li>
+                <li id="social"><a href="#"><i class="fab fa-instagram"></i></a></li>
+                <li id="social"><a href="#"><i class="fab fa-linkedin"></i></a></li>
+            </div>
+
+        </div>
+        <div class="bottom-bar">
+            <p>Copy rights @ EdusparksOnline All rights reserved.
+                <br>Created By : MLB_01.02_01
+
+            </p>
+        </div>
+
+    </footer>
+
+</body>
+
+</html>
--- home.php
+++ home.php
@@ -5,42 +5,163 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>EdusparkOnline - Homapage</title>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
-        rel="stylesheet">
-
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@100..900&display=swap" rel="stylesheet">
-
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.7/css/all.css">
+    <link rel="stylesheet" href="src\styles\style.css">
+    <link rel="stylesheet" href="src\styles\header.css">
+    <link rel="stylesheet" href="src\styles\footer.css">
+
+
 </head>
 
 <body>
 
-    <nav>
-        <div id="logo">
-            <a href="#">
-                <img src="images\logo.png" alt="Logo EdusparkOnline">
-            </a>
+    <?php require 'header.php'; ?>
+
+
+
+    <script src="src\js\home-header.js"></script>
+
+
+    <center>
+
+
+        <div class="container">
+            <div class="images">
+
+                <input type="radio" name="slide" id="slide1" checked>
+                <input type="radio" name="slide" id="slide2">
+                <input type="radio" name="slide" id="slide3">
+                <input type="radio" name="slide" id="slide4">
+
+                <img src="images\slide1.png" alt="slide1" class="s1">
+                <img src="images\slide2.png" alt="slide2" class="s2">
+                <img src="images\slide3.png" alt="slide3" class="s3">
+                <img src="images\slide4.jpg" alt="slide3" class="s4">
+
+            </div>
+
+
+            </d>
+            <div class="dots">
+                <label for="slide1"></label>
+                <label for="slide2"></label>
+                <label for="slide3"></label>
+                <label for="slide4"></label>
+            </div>
+        </div> <button id="become-teacher">Become An Educator</button>
+    </center>
+
+
+
+    <div class="body-content">
+        <h1 class="heading">Our Latest Courses To Explore</h1><br><br>
+        <div class="body">
+            <div class="box" id="course1">
+                <div class="body-img">
+                    <img src="images/courses/ai.png" alt="" width="400px" height="300px">
+                </div>
+                <div class="img-content">
+                    <h3>Artificial Intelligence & Machine Learning</h3>
+                    <p>This course introduces Artificial Intelligence & Machine Learning and how these algorithms could
+                        be applied to general applications.
+                        ★★★★☆</p><br>
+                    <button class="Reg-btn">More Details</button>
+                </div>
+            </div>
+
+            <div class="box" id="course2">
+                <div class="body-img">
+                    <img src="images/courses/java.png" alt="" width="400px" height="300px">
+                </div>
+                <div class="img-content">
+                    <h3>JAVA Development</h3><br><br>
+                    <p>This course provides the base for a versatile and popular programming language used in web and
+                        mobile apps.
+                        <br>
+                        ★★★★☆
+                    </p><br>
+                    <button class="Reg-btn">More Details</button>
+                </div>
+            </div>
+
+            <div class="box" id="course3">
+                <div class="body-img">
+                    <img src="images/courses/graphic.png" alt="" width="400px" height="300px">
+                </div>
+                <div class="img-content">
+                    <h3>Graphics Design</h3><br><br>
+                    <p>This course focuses on designing professional-grade materials using the required knowledge.
+                        <br>
+                        ★★★★☆
+                    </p>
+                    <br>
+                    <button class="Reg-btn">More Details</button>
+                </div>
+            </div>
+
+            <div class="box" id="course4">
+                <div class="body-img">
+                    <img src="images/courses/ui.png" alt="" width="400px" height="300px">
+                </div>
+                <div class="img-content">
+                    <h3>UI/UX Design</h3>
+                    <p>This course helps educators understand the principles of UI/UX design to build professional
+                        applications.
+                        <br>
+                        ★★★★☆
+                    </p><br>
+                    <button class="Reg-btn">More Details</button>
+                </div>
+            </div>
+
+            <div class="box" id="course5">
+                <div class="body-img">
+                    <img src="images/courses/data.png" alt="" width="400px" height="300px">
+                </div>
+                <div class="img-content">
+                    <h3>Data Science</h3>
+                    <p>This course focuses on collecting, analyzing, and interpreting data to improve performance.
+                        <br>
+                        ★★★★☆
+                    </p>
+                    <br>
+                    <button class="Reg-btn">More Details</button>
+                </div>
+            </div>
+
+            <div class="box" id="course6">
+                <div class="body-img">
+                    <img src="images/courses/ps.png" alt="" width="400px" height="300px">
+                </div>
+                <div class="img-content">
+                    <h3>Adobe Photoshop</h3><br>
+                    <p>This course covers advanced Photoshop skills to create professional-grade designs.
+                        <br>
+                        ★★★★☆ <br>
+                    </p><br>
+                    <button class="Reg-btn">More Details</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <center>
+        <div id="events">
+            <img src="images/events.png" alt="" width="100%" height="300px">
+            <button id="load-more">See Upcoming Events</button>
+
         </div>
 
-        <div>
-            <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Courses</a></li>
-                <li><a href="#">Events</a></li>
-                <li><a href="#">About Us</a></li>
-                <li><a href="#">Contact Us</a></li>
-                <li>
-                    <div id="search-icon"></div>
-                    <i class="fas fa-search"></i>
-                </li>
-            </ul>
-        </div>
-    </nav>
+    </center>
+
+    <div id="subbg">
+        <img src="images\subbg.png" alt="sub background" width=100% height="470px">
+    </div>
+
+    <?php include 'footer.php'; ?>
+
+
+</body>
 
 </body>
 
